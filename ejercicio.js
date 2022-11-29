1b94af80f5f88e02e51e525e25c694b757debd8b
SleyAvila <zleysithafc@gmail.com>
2022-11-29 13:55:43 -0500

    Ejercicio 5

---
+++ ejercicio.js
@@ -0,0 +1,38 @@
+/* console.log("PUNTO 1")
+let A=2
+let B=5
+let C=1
+let a,b=0
+a= 3*A - 4*B/A**2
+b= (((B + C) / 2 * A + 10) * 3 * B) - 6
+console.log("a)",a)
+console.log("b)",b) */
+
+/* console.log("PUNTO 2")
+let capital,ganancia = 0
+capital=parseInt(prompt("Digite el capital: "))
+ganancia = capital*0.012
+console.log(`Gana ${ganancia}`) */
+
+/* console.log("PUNTO 3") 
+let base,altura,area=0
+base=parseFloat(prompt("Digite la base del triangulo"))
+altura=parseFloat(prompt("Digite la altura del triangulo"))
+area= base*altura/2
+console.log(`El area del triangulo es \n${area}`)*/
+
+/* console.log("PUNTO 4")
+let salario,cautos,monto=0
+
+cautos=parseInt(prompt("Digite la cantidad de autos que vendio: "))
+monto=parseFloat(prompt("Digite el total del monto de ventas"))
+
+base= 980000
+comision= 170000
+extra = 5
+
+salario = base + (comision*cautos) + (monto*extra/100)
+
+console.log(`El salario total es ${salario}`) */
+
+console.log("PUNTO 5")
